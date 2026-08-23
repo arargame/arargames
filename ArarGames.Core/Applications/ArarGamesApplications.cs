a13f31c96fd1bf70462a2042ec3b81ebbe0f327d
koray arar <arargame@hotmail.com>
2026-08-24 01:54:03 +0300

    Add shared application catalogue

---
+++ ArarGames.Core/Applications/ArarGamesApplications.cs
@@ -0,0 +1,10 @@
+namespace ArarGames.Core.Applications;
+
+/// <summary>Canonical product catalogue shared by Arar Games titles.</summary>
+public static class ArarGamesApplications
+{
+    public const string CoffeeProductId = "buy_me_a_coffee";
+    public const string BlockedGooglePlay = "https://play.google.com/store/apps/details?id=com.arargames.blocked";
+    public const string PaintTrekGooglePlay = "https://play.google.com/store/apps/details?id=com.arargame.PaintTrek.Android";
+    public const string PaintTrekMicrosoftStore = "https://apps.microsoft.com/detail/9wzdncrdpznh?cid=DevShareMTwPCBIL";
+}
